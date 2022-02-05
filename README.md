486fee69aaf033cd28ac43fc141198ec5e7f062f
= Aminu Ahmadh <= ameenuhamad@gmail.com>
2022-02-05 13:46:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# CRYPTO_ALERT: Dip, Surge and Trend
+
+This app will notify you of bitcoin or ethereum massive drop or gain via Text message and telegram. It would also sends you the top ten coins trending on https://Apewisdom.io 
+
+This project isn't done yet. It's a work in progress. will continue updating. Please let me know a specific feature you'll want me to add or if you find any issue with the code.
+
+
+## First
+
+The `requirements.txt` file should list all Python libraries that the script depend on, and you should install it using:
+
+""" pip install -r requirements.txt """
+
+
+## Second
+
+You have to register an account with vonage to get access to their SMS API. So head over to https://dashboard.nexmo.com/ to register and account and collect your KEY and SECRET for client authentication.
+Secondly, head over to https://api.coinmarketcap.com to get your free API key for crypto data inquiry.
+
+## Third
+
+An Email plus password is required by the library `yagmail` to send yourself Email. You can create a new gmail just for that purpose or use your own. And for it to work, you have to turn on Less secure app on https://accounts.google.com for that account that you'll use to send yourself email.
+
+## Fourth
+
+You have to create a bot on telegram using @BotFather or use one if you already have it. That bot will be used to send yourself message on telegram.
+
+## Lastly
+
+You should put all your credentials (APIs, Secret keys, email and pass) into `.env` file and access them using enviromental variable so that nobody can have access to your credentials.
+
+## Happy Coding...
---
+++ crypto_alert.py
@@ -0,0 +1,125 @@
+# import the required libraries
+
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+import json
+from datetime import datetime
+import yagmail
+import coinmarketcapapi
+from pretty_html_table import build_table
+import vonage
+
+# load environmental variables
+load_dotenv()
+
+MY_TOKEN = os.getenv("MY_TOKEN")
+API_KEY = os.getenv('COIN_API_KEY')
+RECIPENT_EMAIL = os.getenv("RECIPENT_EMAIL")
+MAIL_FROM_PASS = os.getenv("MESSAGE_FROM_PASS")
+KEY = os.getenv("KEY")
+SECRET = os.getenv("SECRET")
+EMAIL_FROM = os.getenv("EMAIL_FROM")
+SMS_TO = os.getenv("TO_SMS")
+
+# instantiate the CMC api
+cmc = coinmarketcapapi.CoinMarketCapAPI(API_KEY)
+
+# setting up client for VONAGE TEXT messages
+client = vonage.Client(key=KEY, secret=SECRET)
+sms = vonage.Sms(client)
+
+# function to send emails
+def send_emails(contents):
+    global RECIPENT_EMAIL
+    recipent_email_address = RECIPENT_EMAIL
+    global MAIL_FROM_PASS
+    password1 = MAIL_FROM_PASS
+    msg_from = EMAIL_FROM
+    yag = yagmail.SMTP(msg_from, password1)
+    content = ["<h1> TODAY'S TREND... </h1>", contents + '<h1> End Of Message </h1>']
+    yag.send(to=recipent_email_address, subject= '[Automated Email] Apes Trend: ' + 
+    str(datetime.now().date()), contents=content)
+
+
+# send updates via telegram
+def send_telegram(message):
+    requests.post(
+        'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=markdown'.format(MY_TOKEN, 786259592, message))
+
+
+def get_apes_trend():
+    apes_wisdom = 'https://apewisdom.io/api/v1.0/filter/all-crypto'
+    response = requests.get(apes_wisdom).text
+    result = json.loads(response)
+    Ranks, Tickers, Names, Mentions, Upvotes = [], [], [], [], []
+    for i in range(10):
+        # get the top 10 TRENDING COINS
+        Ranks.append(result['results'][i]['rank'])
+        Tickers.append(result['results'][i]['ticker'])
+        Names.append(result['results'][i]['name'])
+        Mentions.append(result['results'][i]['mentions'])
+        Upvotes.append(result['results'][i]['upvotes'])
+
+    df = pd.DataFrame({
+        "Ranks": Ranks,
+        "Tickers": Tickers,
+        "Names": Names,
+        "Mentions": Mentions,
+        'Upvotes': Upvotes}, index=Ranks)
+
+    result_prettified = build_table(df, 'blue_light')
+    return result_prettified
+    
+
+def bear_market():
+    # returns true if bear market (btc/eth down 15% or more in 24hr or 20% or more last 7 days)
+    return (btc_change_24_hr <= -15 or eth_change_24_hr <= -15) or (btc_change_7days <= -20 or eth_change_7days <= -20)
+def bull_market():
+    # returns true if bull market (btc/eth up 15% or more in 24hr or 20% or more last 7 days)
+    return (btc_change_24_hr >= 15 or eth_change_24_hr >= 15) or (btc_change_7days >= 20 or eth_change_7days >= 20)
+
+
+btc = cmc.cryptocurrency_quotes_latest(symbol='BTC', convert='USD')
+eth = cmc.cryptocurrency_quotes_latest(symbol='ETH', convert='USD')
+btc_change_7days = btc.data['BTC']['quote']['USD']['percent_change_7d']
+btc_change_24_hr =  btc.data['BTC']['quote']['USD']['percent_change_24h']
+eth_change_7days = eth.data['ETH']['quote']['USD']['percent_change_7d']
+eth_change_24_hr = eth.data['ETH']['quote']['USD']['percent_change_24h']
+
+if __name__ == "__main__":
+
+    print('Sending Email')
+    send_emails(get_apes_trend())
+    print('Email Sent')
+
+    if bear_market():
+        send_telegram('Seems Like We Are Entering a Bear Market!')
+        responseData = sms.send_message({
+        "from": "Bully",
+        "to": SMS_TO,
+        "text": "Buy The Fucking Dip!!!",})
+
+        if responseData["messages"][0]["status"] == "0":
+            print("Message sent successfully.")
+        else:
+            print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+        print('sent bear market notification')
+
+    elif bull_market():
+        send_telegram('Bull Market Happening baby!!!')
+        responseData = sms.send_message({
+        "from": "Bully",
+        "to": SMS_TO,
+        "text": "Bull Market Baby!!!",})
+
+        if responseData["messages"][0]["status"] == "0":
+            print("Message sent successfully.")
+        else:
+            print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+        print('sent bull market notification')
+
+    print('No bear or bull Market')
+
+print('Done for the day', str(datetime.now()))
